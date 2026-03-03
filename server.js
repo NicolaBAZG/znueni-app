505061501e2de4c32b396bcc4ffc01936da5da62
NicolaBAZG <nicola.vaucher@sword-group.com>
2026-03-03 11:47:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const DATA_FILE = path.join(__dirname, 'data.json');
 const MAX_ZNUENI = 4;
